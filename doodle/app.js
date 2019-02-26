62cf7a3e1953d575aee7d9267df6c47f3039a7d7
Olivier Valette <olivier.valette@spi10.com>
2019-02-26 14:12:32 +0100

    adding middleware

--- doodle/app.js
+++ doodle/app.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-//
+// define parameter
 app.set('view engine', 'pug');
+
+// middleware to define asset directory (public) and its path (./public)
+app.use('/public', express.static('public'));
+
+// middleware for forms type management
+// Parser application/json
+app.use(bodyParser.json());
+// Parser application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// middleware example : ip address of connected user
+app.use((req, res, next) => {
+    console.log(req.ip);
+    next();
+});
+
+// routing
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Accueil', username: 'OlivierValette'});
+});
+
+app.get('/doodle/:id', (req, res) => {
+    const { id } = req.params;
+    res.render('doodle', { id: id});
 });
 
 app.listen(8000);
