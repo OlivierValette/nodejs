34d8d2a140057d0284d8960d33b3e3b1d069d892
Olivier Valette <olivier.valette@spi10.com>
2019-02-26 11:38:02 +0100

    pug install

--- doodle/app.js
+++ doodle/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
+//
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.render('index', { username: 'OlivierValette'});
 });
 
 app.listen(8000);
